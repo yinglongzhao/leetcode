c18b31fc86329a8999f878997c823a57313e0682
zhaoyinglong <zhaoyl3@qq.com>
2025-02-21 14:48:54 +0800

    feat: 034 golang算法提交

--- solution/0000-0099/0034.Find First and Last Position of Element in Sorted Array/Solution.go
+++ solution/0000-0099/0034.Find First and Last Position of Element in Sorted Array/Solution.go
@@ -6,3 +6,43 @@ func searchRange(nums []int, target int) []int {
 	}
 	return []int{l, r - 1}
 }
+
+func searchRange(nums []int, target int) []int {
+	if len(nums) == 0 {
+		return []int{-1, -1}
+	}
+	left := searchLeftIndex(nums, target)
+	if left == nums[len(nums)-1] || nums[left] != target {
+		return []int{-1, -1}
+	}
+	right := searchLeftIndex(nums, target)
+	return []int{left, right}
+}
+
+func searchLeftIndex(nums []int, target int) int {
+	l, r := 0, len(nums)
+
+	for l < r {
+		mid := l + (r-l)/2
+		if nums[l] < target {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+	return l
+}
+
+func searchRightIndex(nums []int, target int) int {
+	l, r := 0, len(nums)
+
+	for l < r {
+		mid := l + (r-l)/2
+		if nums[l] <= target {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+	return l
+}
